29252cb35ebc3a7b62d752b1b0a67262caee5c7f
Fissnik <ffisnikmaloku@gmail.com>
2022-01-23 22:21:31 +0100

    init

---
+++ Client_/Client.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+using static Client_.Delegates;
+
+namespace Client_
+{
+   public class Delegates
+   {
+      public delegate void OnMessageReceive(string message);
+      public delegate void ClientDisconnected();
+   }
+
+   public class Client
+   {
+      private readonly TcpClient _tcpClient;
+      private readonly Action<string> OnMessageReceive;
+
+      public Client(string ipAddress, int port, Action<string> onMessageReceive)
+      {
+         _tcpClient = new TcpClient(ipAddress, port);
+         OnMessageReceive = onMessageReceive;
+      }
+
+      public void SendMessage(string message)
+      {
+         var byteData = Encoding.ASCII.GetBytes(message);
+         var stream = _tcpClient.GetStream();
+         stream.Write(byteData, 0, byteData.Length);
+         Console.ForegroundColor = ConsoleColor.DarkGreen;
+         Console.WriteLine($"Me: {message}");
+         Console.ForegroundColor = ConsoleColor.DarkRed ;
+      }
+
+      public void CheckForMessages()
+      {
+         try
+         {
+            var bytes = new Byte[256];
+            var stream = this._tcpClient.GetStream();
+            int i;
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+               var data = Encoding.ASCII.GetString(bytes, 0, i);
+               OnMessageReceive?.Invoke(data);
+            }
+         }
+         catch (Exception)
+         {
+            
+         }
+      }
+
+   }
+
+
+}
---
+++ Client_/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Client_
+{
+   class Program
+   {
+      static Client client;
+      static void Main(string[] args)
+      {
+         client = new Client("127.0.0.1", 81, OnMessageReceive);
+         Console.WriteLine("Write a message to server: ");
+         Task.Run(client.CheckForMessages);
+
+         while (true)
+         {
+            var message = Console.ReadLine();
+            if (message.ToLower() == "exit")
+               break;
+            if (!string.IsNullOrEmpty(message))
+            {
+               client.SendMessage(message);
+            }
+         }
+         Console.Read();
+      }
+
+      static void OnMessageReceive(string message)
+      {
+         Console.ForegroundColor = ConsoleColor.DarkMagenta;
+         Console.WriteLine($"Server: {message}");
+         Console.ForegroundColor = ConsoleColor.DarkRed;
+      }
+
+   }
+}
+
